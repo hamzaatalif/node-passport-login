d9cba5a6e6fb8ca1ef90dd43445fcff3db21ad09
hamzaatalif <hamzaatalif@gmail.com>
2021-09-04 03:36:17 +0500

    register almost

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const bcrypt = require("bcrypt");
 
 const users = [];
 
@@ -25,8 +26,20 @@ app.get("/register",(req,res)=>{
     res.render("register.ejs")
 })
 
-app.post("/register",(req,res)=>{
-
+app.post("/register", async (req,res)=>{
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect("/login")
+    } catch (error) {
+        res.redirect("/register")
+    }
+    console.log(users);
 })
 
 
