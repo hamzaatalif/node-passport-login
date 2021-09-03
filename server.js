c76de207697ef4bb7c52ea4beaadbd6e6ea9822d
hamzaatalif <hamzaatalif@gmail.com>
2021-09-04 03:30:55 +0500

    basic functionality

--- server.js
+++ server.js
@@ -2,12 +2,33 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const users = [];
+
 app.set("view-engine","ejs")
+
+app.use(express.urlencoded({extended: false}))
 
 
 app.get("/",(req,res)=>{
     res.render("index.ejs", {name: "Hamza"});
 })
+
+app.get("/login",(req,res)=>{
+    res.render("login.ejs")
+})
+
+app.post("/login",(req,res)=>{
+    
+})
+
+app.get("/register",(req,res)=>{
+    res.render("register.ejs")
+})
+
+app.post("/register",(req,res)=>{
+
+})
+
 
 
 
